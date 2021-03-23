cda51151980d4992377d0c0526b761a02bc29ec4
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-23 16:38:08 +0530

    drum

--- DrumSticks/index.js
+++ DrumSticks/index.js
@@ -1,16 +1,18 @@
-function removeTransition(e){
-    if(e.propertyName !== 'transform')return;
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-}
-function audioPlay(e){
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    if(!audio) return;
+  }
+
+  function playaudio(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
     key.classList.add('playing');
-    audio.currentTime =0;
+    audio.currentTime = 0;
     audio.play();
-}
-const keys = Array.from(document.querySelectorAll('.key'));
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+  window.addEventListener('keydown', playaudio);
