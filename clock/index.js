ab6c8218549f6410758d1773dce35ec7b23cec4c
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-23 16:15:50 +0530

    js

--- clock/index.js
+++ clock/index.js
@@ -6,11 +6,9 @@ setInterval(() => {
     hrotation = 30*htime + mtime/2;
     mrotation = 6*mtime;
     srotation =  6*stime;
-
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
-
 },1000);
 
 // setInterval(() => {
