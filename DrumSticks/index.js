386054b5bddbec12469736b400a5ac0314318632
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-24 21:24:04 +0530

    drums sticks

--- DrumSticks/index.js
+++ DrumSticks/index.js
@@ -1,11 +1,11 @@
-function removeTransition(e) {
+function changeTarnsition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
   }
 
   function playaudio(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[audio-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[audio-key="${e.keyCode}"]`);
     if (!audio) return;
 
     key.classList.add('playing');
@@ -14,5 +14,5 @@ function removeTransition(e) {
   }
 
   const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  keys.forEach(key => key.addEventListener('transitionend', changeTarnsition));
   window.addEventListener('keydown', playaudio);
